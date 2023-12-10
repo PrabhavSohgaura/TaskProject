a94424185b430447acb253ffe46530294c6d8b45
PrabhavSohgaura <prabhavsohgaura333@gmail.com>
2023-12-10 18:28:44 +0530

    added the source folder

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectSiderbar from "./components/ProjectSiderbar";
+
+function App() {
+  return (
+    <main className="h-screen my-2">
+      <ProjectSiderbar />
+    </main>
+  );
+}
+
+export default App;
