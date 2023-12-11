3a04dfe4a8e613c4a2fdbbbb4712224607ef5847
PrabhavSohgaura <prabhavsohgaura333@gmail.com>
2023-12-11 09:05:21 +0530

    added the button

--- src/components/ProjectSiderbar.jsx
+++ src/components/ProjectSiderbar.jsx
@@ -8,7 +8,7 @@ const ProjectSiderbar = () => {
       </h2>
       <div>
         <button className="px-4 py-2 text-xs md: text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-500 hover:text-stone-200">
-          Add Project
+          + Add Project
         </button>
       </div>
       <ul></ul>
